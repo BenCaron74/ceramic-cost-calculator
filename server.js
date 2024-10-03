10bb5c9d6d2a866c50816bc93cc8c111d2e25c4b
ben caron <bencaron@MacBook-Pro-4.local>
2024-10-03 19:40:34 +0200

    vfdvfd

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/cost_calcula
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
+  serverSelectionTimeoutMS: 30000,  // 30 secondes de délai d'attente
 })
   .then(() => console.log('MongoDB connecté'))
   .catch(err => console.log('Erreur de connexion MongoDB :', err));
